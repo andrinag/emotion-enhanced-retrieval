a6edbde1986cad0291931d09a73d90d979b1924a
Andrina Geller <andrina.geller@unibas.ch>
2025-01-29 15:05:48 +0100

    add new structure

--- SER/Android App/app/build.gradle.kts
+++ SER/Android App/app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("com.google.ar:core:1.47.0")
+    implementation("com.affecitva.android:")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
