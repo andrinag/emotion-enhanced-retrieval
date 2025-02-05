8a5a97bbaab0254a9dbc7ca085f19a888a38d010
Andrina Geller <andrina.geller@unibas.ch>
2025-02-05 10:30:39 +0100

    make that it doesnt open all the videos at once

--- SER/Retrieval Engine/upload_videos.py
+++ SER/Retrieval Engine/upload_videos.py
@@ -16,14 +16,12 @@ def get_video_list_from_folder(folder_path):
 
 
 if __name__ == "__main__":
-    # uploading videos, still needs to be changed for a better dataset
     folder_path = "/media/V3C/V3C1/video-480p"
     video_list = get_video_list_from_folder(folder_path)
 
     url = "http://127.0.0.1:8000/upload_videos/"
-    video_files = []
-    for i in range(len(video_list)):
-        video_files.append(("files", open(video_list[i], "rb")))
 
-    response = requests.post(url, files=video_files)
-    print(response.json())
+    for video_path in video_list:
+        with open(video_path, "rb") as video_file:
+            response = requests.post(url, files={"files": video_file})
+            print(response.json())
