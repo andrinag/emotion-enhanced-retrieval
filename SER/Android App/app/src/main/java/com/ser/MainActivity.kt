2d052486321efd9aee185ab2417685e66c9c15a6
Andrina Geller <andrina.geller@unibas.ch>
2025-02-12 15:24:09 +0100

    delete frames

---
+++ SER/Android App/app/src/main/java/com/ser/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @Override
+    protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
