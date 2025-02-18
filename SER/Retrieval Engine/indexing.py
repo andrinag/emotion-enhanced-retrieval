5046d1a24b5406eaf5fd3db67f76ffbe9f4d35c1
Andrina Geller <andrina.geller@unibas.ch>
2025-02-18 15:40:11 +0100

    update indexing for database

--- SER/Retrieval Engine/indexing.py
+++ SER/Retrieval Engine/indexing.py
@@ -6,14 +6,12 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-
-# connection to the local database
 conn = psycopg2.connect(
     dbname="multimedia_db",
     user="test",
     host="localhost",
     password="123",
-    port="5432"
+    port="5433"
 )
 register_vector(conn)
 
@@ -22,36 +20,25 @@ register_vector(conn)
 async def create_index_hnsw():
     """
     Create an hnsw index on the embeddings (vectors) column of the multimedia_embeddings table.
-    TODO: change to the correct parameters
     """
     try:
         cursor = conn.cursor()
+        cursor.execute("CREATE EXTENSION IF NOT EXISTS vector;")
         cursor.execute("""
-            CREATE INDEX idx_embeddings_hnsw ON embeddings 
-            USING hnsw (content_vector vector_l2_ops) WITH (m = 10, ef_construction = 40);
+            CREATE INDEX IF NOT EXISTS idx_embedding_hnsw 
+            ON multimedia_embeddings 
+            USING hnsw (embedding vector_l2_ops) 
+            WITH (m = 10, ef_construction = 40);
         """)
+
+        conn.commit()
         cursor.close()
+
+        return {"message": "HNSW index created successfully"}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-
-@app.get("/ivfflat_index")
-async def create_index_ivfflat():
-    """
-    Create an hnsw index on the embeddings (vectors) column of the multimedia_embeddings table.
-    TODO: change to the correct parameters
-    """
-    try:
-        cursor = conn.cursor()
-        cursor.execute("""
-            CREATE INDEX idx_embeddings_ivfflat ON embeddings USING ivfflat (embedding vector_cosine_ops) WITH (lists = 1000);
-        """)
-        cursor.close()
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
