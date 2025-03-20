50b545796909dd48ebddfd1a4ba917ded93d7c62
Andrina Geller <andrina.geller@unibas.ch>
2025-03-20 08:31:22 +0100

    add new db schema for emotions and a proper requirements file

--- SER/Retrieval Engine/multimedia_schema.sql
+++ SER/Retrieval Engine/multimedia_schema.sql
@@ -1,31 +1,40 @@
 CREATE TABLE multimedia_objects (
 object_id SERIAL PRIMARY KEY,
-type VARCHAR(30),
 location VARCHAR(255)
 );
 
 CREATE TABLE multimedia_embeddings (
 id SERIAL PRIMARY KEY,
 object_id INTEGER,
+frame INTEGER,
 frame_time FLOAT,
-embedding vector(512),
-
-emotion_voice VARCHAR(255),
-emotion_percentage_voice FLOAT,
-sentiment_voice VARCHAR(255),
-
-emotion_face VARCHAR(255),
-emotion_percentage_face FLOAT,
-sentiment_face VARCHAR(255),
-
-emotion_ocr VARCHAR(255),
-emotion_percentage_ocr FLOAT,
-sentiment_ocr VARCHAR(255),
-FOREIGN KEY(object_id) REFERENCES multimedia_objects(object_id)
+embedding vector(512)
 );
 
-CREATE TABLE EMOTIONS (
+CREATE TABLE OCR (
 id SERIAL PRIMARY KEY,
-date_time TIMESTAMP,
-emotion VARCHAR(255)
+emotion VARCHAR(30),
+confidence FLOAT,
+sentiment VARCHAR(30),
+path_annotated_location VARCHAR(255),
+y_axis1 FLOAT,
+x_axis1 FLOAT,
+y_axis2 FLOAT,
+x_axis2 FLOAT
+)
+
+CREATE TABLE Face (
+id SERIAL PRIMARY KEY,
+emotion VARCHAR(30),
+confidence FLOAT,
+sentiment VARCHAR(30),
+path_annotated_faces VARCHAR(255)
+)
+
+CREATE TABLE ASR (
+id SERIAL PRIMARY KEY,
+text VARCHAR(50000),
+emotion VARCHAR(30),
+confidence FLOAT,
+sentiment VARCHAR(30)
 )
