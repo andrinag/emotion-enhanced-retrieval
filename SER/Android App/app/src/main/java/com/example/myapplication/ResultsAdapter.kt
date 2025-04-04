8aa72ed0315c4120b0c8f1240360d1a59527b178
Andrina Geller <andrina.geller@unibas.ch>
2025-04-04 10:12:31 +0200

    pause/play button for videos (still has scroll issues)

--- SER/Android App/app/src/main/java/com/example/myapplication/ResultsAdapter.kt
+++ SER/Android App/app/src/main/java/com/example/myapplication/ResultsAdapter.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -26,6 +29,7 @@ class ResultsAdapter(
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val videoView: VideoView = itemView.findViewById(R.id.videoItemView)
         val imageView: ImageView = itemView.findViewById(R.id.annotationImage)
+        val mediaController: MediaController = MediaController(itemView.context)
     }
 
     /**
@@ -42,18 +46,40 @@ class ResultsAdapter(
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder.videoView.setVideoURI(Uri.parse(item.videoUrl))
+        val videoUri = Uri.parse(item.videoUrl)
+
+        holder.videoView.setVideoURI(videoUri)
+
         holder.videoView.setOnPreparedListener { mediaPlayer ->
-            mediaPlayer.seekTo((item.frameTime * 1000).toInt())
-            mediaPlayer.start()
+            val seekToMs = (item.frameTime * 1000).toInt()
+            holder.videoView.seekTo(seekToMs)
+
+            // Attach MediaController AFTER prepared, else there is error playing the video!
+            val mediaController = MediaController(context)
+            mediaController.setAnchorView(holder.videoView)
+            holder.videoView.setMediaController(mediaController)
+        }
+
+        holder.videoView.setOnCompletionListener {
+            Log.i("VIDEO", "Playback completed")
+        }
+
+        holder.videoView.setOnErrorListener { _, what, extra ->
+            Log.e("VIDEO", "Error playing video: $videoUri - what: $what extra: $extra")
+            Toast.makeText(context, "Error playing video", Toast.LENGTH_SHORT).show()
+            true
         }
 
         item.annotatedImageUrl?.let {
             Glide.with(context)
                 .load(it)
                 .into(holder.imageView)
+            holder.imageView.visibility = View.VISIBLE
+        } ?: run {
+            holder.imageView.visibility = View.GONE
         }
     }
+
 
     override fun getItemCount(): Int = items.size
 }
