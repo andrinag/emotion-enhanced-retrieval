34180371ef4404c7a4852112a255efb78695217d
Andrina Geller <andrina.geller@unibas.ch>
2025-07-21 15:14:54 +0200

    add a few more (and better) compliments

--- EER/Backend/cheerup_api.py
+++ EER/Backend/cheerup_api.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
 import pyjokes
+import random
 
 app = FastAPI()
 
+compliments = [
+    "You have such nice hair!",
+    "You have a great smile!",
+    "You light up the world!",
+    "You make the world a better place just by being in it!",
+    "Your kindness is contagious!",
+    "You have great sense of humour!"
+]
 
 @app.get("/joke")
 async def get_joke():
@@ -17,7 +26,7 @@ async def get_joke():
 @app.get("/compliment")
 async def get_joke():
     try:
-        return {"compliment": "You have such nice hair!"}
+        return {"compliment": random.choice(compliments)}
     except Exception as e:
         return {"error": str(e)}
 
